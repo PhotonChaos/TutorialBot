d050fdb93c92b0f523a779d71586307739a514ed
Carbon <calumgcoppack@gmail.com>
2018-02-24 17:22:42 -0500

    Added lesson on variables

--- TutorialBot/Modules/BasicModule.cs
+++ TutorialBot/Modules/BasicModule.cs
@@ -10,7 +10,10 @@ using Discord.Commands;
 namespace TutorialBot.Modules // Namespace MUST be <namespace>.Modules
 {
     // module classes MUST be explicitly marked as public
-    public class BasicModule : ModuleBase<SocketCommandContext> {  
+    public class BasicModule : ModuleBase<SocketCommandContext> {
+        private static int number; // Module class instances only last as long as the command has been called, theya re destroyed once a method ends
+                                   // So you should make variables static if you want them to persist after the command has ended.
+
         [Command("ping")] // the name of the command that you will use in discord
         [Name("Ping")] // The display name of the command (used for making a help command)
         [Summary("Responds with pong")] // This is the summary of the command (also used for the help command)
@@ -34,6 +37,15 @@ namespace TutorialBot.Modules // Namespace MUST be <namespace>.Modules
             }
 
             await ReplyAsync($"{Context.Message.Author} says {s}"); // the Context object holds all information about the context of the command (it holds the message object, etc.)
+        }
+
+        [Command("add")]
+        [Name("Add")]
+        [Summary("Adds to a variable")]
+        public async Task AddAsync(int num = 0) {
+            // Disciord.NET will interpret numbers as an int if the parameter is of type int
+            number += num;
+            await ReplyAsync($"Added {num} to number, number is now {number}.");
         }
     }
 }
